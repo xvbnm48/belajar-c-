218881f4fe6e470ad14b58bf9d191b705489cdff
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-03-16 23:21:11 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+string nama;
+string umur = Convert.ToInt32;
+Console.WriteLine("masukkan umur");
+umur = Console.ReadLine();
+
+Console.WriteLine("Masukkan nama anda: ");
+// Console.WriteLine(umur);
+// nama = Console.ReadLine();
+Console.WriteLine("Halo, " + nama);
+
+// Console.WriteLine("Hello " + nama);
